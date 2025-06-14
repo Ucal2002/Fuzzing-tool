0c7047f06e3111262215dab42aa4886009ea9fb3
Luca Mitercu <luca.mitrescu@gmail.com>
2025-06-15 00:56:28 +0200

    Add initial fuzzer script and startup configuration

--- fuzzer.py
+++ fuzzer.py
@@ -4,6 +4,16 @@ from executor import send_request
 from analyzer import analyze
 from logger import log
 
+print(""" @@@@@@@@ @@@  @@@ @@@@@@@@ @@@@@@@@ @@@@@@@@ @@@@@@@ 
+ @@!      @@!  @@@      @@!      @@! @@!      @@!  @@@
+ @!!!:!   @!@  !@!    @!!      @!!   @!!!:!   @!@!!@! 
+ !!:      !!:  !!!  !!:      !!:     !!:      !!: :!! 
+  :        :.:: :  :.::.: : :.::.: : : :: :::  :   : :
+                                                      """)
+print("Please enter your Fuzzing configuration:")
+input("\n[↩]  press Enter, to start fuzzing from the default configuration file...")
+
+
 with open("config.json") as f:
     config = json.load(f)
 
@@ -15,3 +25,8 @@ for seed in seeds:
     response = send_request(payload, config)
     if analyze(response, payload):
         log(payload, response)
+
+
+    print(f"[+] Fuzzing with payload: {payload}")
+    print(f"    Status: {response.status_code}")
+    print(f"    Body: {response.text[:100]}")
